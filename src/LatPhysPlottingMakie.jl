8c7746d2e6b86c6f8f6b4ecb39c66f76d18e2cc3
F Freyer <frederic481994@hotmail.de>
2019-12-14 11:40:44 +0100

    Files generated by PkgTemplates

---
+++ src/LatPhysPlottingMakie.jl
@@ -0,0 +1,5 @@
+module LatPhysPlottingMakie
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using LatPhysPlottingMakie
+using Test
+
+@testset "LatPhysPlottingMakie.jl" begin
+    # Write your own tests here.
+end
